f6522be3e538f6f38bad97703ae7d3121888cfa5
Balaji Ramasubramanian <balajiram@cpan.org>
2019-01-21 18:34:04 -0800

    initial commit

---
+++ lib/Circuit/Spice.pm
@@ -0,0 +1,19 @@
+use warnings;
+use strict;
+package Circuit::Spice;
+
+# ABSTRACT: brief summary of what this module is used for
+
+use Exporter 'import';
+our (@EXPORT_OK) = ();
+our (@EXPORT)    = (@EXPORT_OK);
+
+=head1 SYNOPSIS
+
+    use Circuit::Spice;
+
+    ## ... code that uses your package
+
+=cut
+
+1;
